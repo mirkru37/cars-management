016dd64c8cbb51d4ca4e67616dea67d2e2a56d54
Roman <mirkru37@gmail.com>
2021-11-28 17:39:44 +0200

    Add colorized output

--- lib/output.rb
+++ lib/output.rb
@@ -1,4 +1,5 @@
 require 'terminal-table'
+require 'colorize'
 
 class Output
   @search_result_table_width = 80
@@ -7,28 +8,28 @@ class Output
 
   # @param [Search] search
   def self.search_statistic(search)
-    table = Terminal::Table.new title: 'Statistic'
+    table = Terminal::Table.new title: 'Statistic'.colorize(color: :light_green).bold
     set_search_table_style(table)
-    table << ['Total quantity', search.total_quantity]
-    table << ['Request quantity', search.request_quantity]
+    table << ['Total quantity'.colorize(:green), search.total_quantity.to_s.colorize(:green).italic]
+    table << ['Request quantity'.colorize(:green), search.request_quantity.to_s.colorize(:green).italic]
     table.align_column(1, :right)
     puts table
   end
 
   # @param [Terminal::Table] table
   def self.set_search_table_style(table)
-    table.style = { width: @search_result_table_width, border_bottom: false, border_x: '=', border_i: 'x'}
+    table.style = { width: @search_result_table_width, border_bottom: false, border_x: '=', border_i: 'x' }
   end
 
   # @param [Array<Car>] result
   def self.search_result(result)
     table = Terminal::Table.new
-    table.title = 'Result'
-    table.headings = %w[Field Information]
+    table.title = 'Result'.colorize(:light_magenta).bold
+    table.headings = ['Field'.colorize(:light_magenta).bold, 'Information'.colorize(:light_magenta).bold]
     set_search_table_style(table)
     result.each do |item|
       item.attributes.each do |key, value|
-        table << [key, value]
+        table << [key.to_s.colorize(:light_magenta), value.to_s.colorize(:magenta).italic]
       end
       table << :separator
     end
