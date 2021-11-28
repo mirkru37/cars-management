b3b366c353e77a517d0a1b9d176eb3de2601044d
Roman <mirkru37@gmail.com>
2021-11-28 15:10:47 +0200

    Add output.rb
    class for printing information

---
+++ lib/output.rb
@@ -0,0 +1,19 @@
+
+class Output
+
+  # @param [Search] search
+  def self.search_statistic(search)
+    puts 'Statistic:', '',
+         "\tTotal quantity: #{search.total_quantity}", '',
+         "\tRequest quantity: #{search.request_quantity}", ''
+  end
+
+  # @param [Array<Car>] result
+  def self.search_result(result)
+    puts 'Result:', ''
+    result.each do |item|
+      puts item
+      puts
+    end
+  end
+end
