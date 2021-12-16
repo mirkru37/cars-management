5231514afe08aee1fe076d2fa37e248ec66a1406
Roman <mirkru37@gmail.com>
2021-12-16 19:23:23 +0200

    class method method changed

--- lib/i18n_config.rb
+++ lib/i18n_config.rb
@@ -1,11 +1,13 @@
 class I18nConfig
-  def self.init
-    I18n.load_path << Dir["#{File.expand_path('config/locales')}/*.yml"]
-    I18n.default_locale = :en
-  end
+  class << self
+    def init
+      I18n.load_path << Dir["#{File.expand_path('config/locales')}/*.yml"]
+      I18n.default_locale = :en
+    end
 
-  def self.choose_language(locales)
-    locale = Input.option(locales, default: 'en', message: I18n.t('input.request.language'))
-    I18n.locale = locale
+    def choose_language(locales)
+      locale = Input.option(locales, default: 'en', message: I18n.t('input.request.language'))
+      I18n.locale = locale
+    end
   end
 end
--- lib/style/table.rb
+++ lib/style/table.rb
@@ -2,15 +2,17 @@
 
 module Style
   class Table
-    # @param [Terminal::Table] table
-    def self.config(table, table_width)
-      table.style = { width: table_width, border_bottom: false, border_x: '=', border_i: 'x' }
-    end
+    class << self
+      # @param [Terminal::Table] table
+      def config(table, table_width)
+        table.style = { width: table_width, border_bottom: false, border_x: '=', border_i: 'x' }
+      end
 
-    def self.empty_table
-      Terminal::Table.new title: Text.title('empty_table', color: :light_red) do |table|
-        table << [' ']
-        table << :separator
+      def empty_table
+        Terminal::Table.new title: Text.title('empty_table', color: :light_red) do |table|
+          table << [' ']
+          table << :separator
+        end
       end
     end
   end
