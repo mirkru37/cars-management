643c88a33bf8b780e3903d58bdd7da2d2950a8be
Roman <mirkru37@gmail.com>
2021-12-29 08:44:33 +0200

    Menu hash layout logic changed

--- lib/app.rb
+++ lib/app.rb
@@ -4,19 +4,16 @@ class App
   MAIN_MENU_ATTRS = {
     search_car: :search_car,
     show_all_cars: :show_all,
-    log_in: :log_in,
-    sign_up: :sign_up,
     help: :help_main,
     change_language: :change_language,
     exit: :close
   }.freeze
-  MAIN_MENU_LOGGED_ATTRS = {
-    search_car: :search_car,
-    show_all_cars: :show_all,
-    log_out: :log_out,
-    help: :help_main,
-    change_language: :change_language,
-    exit: :close
+  MAIN_MENU_AUTHORIZED_ATTRS = {
+    log_out: :log_out
+  }.freeze
+  MAIN_MENU_UNAUTHORIZED_ATTRS = {
+    log_in: :log_in,
+    sign_up: :sign_up
   }.freeze
   LOCALES = %w[uk en].freeze
 
@@ -33,7 +30,7 @@ class App
   end
 
   def main_menu
-    @user.nil? ? map_menu(MAIN_MENU_ATTRS) : map_menu(MAIN_MENU_LOGGED_ATTRS)
+    @user.nil? ? map_menu(main_menu_unauthorized) : map_menu(main_menu_authorized)
   end
 
   def user=(value)
@@ -44,6 +41,15 @@ class App
 
   private
 
+  def main_menu_authorized
+    MAIN_MENU_ATTRS.to_a.insert(2, *MAIN_MENU_AUTHORIZED_ATTRS.to_a).to_h
+  end
+
+  def main_menu_unauthorized
+    MAIN_MENU_ATTRS.to_a.insert(2, *MAIN_MENU_UNAUTHORIZED_ATTRS.to_a).to_h
+  end
+
+  # @param [Hash] menu_attrs
   def map_menu(menu_attrs)
     menu_attrs.map { |item, option| Models::MenuItem.new(item, Menu::Options.method(option)) }
   end
