1a87239cd5bd99456a2c1aaa9a779d2631c79c20
Roman <mirkru37@gmail.com>
2022-01-11 10:48:15 +0200

    `user_searches` refactor

--- lib/console/output/user_searches.rb
+++ lib/console/output/user_searches.rb
@@ -18,27 +18,27 @@ module Output
       def fill_searches(searches, email)
         Terminal::Table.new do |table|
           table.title = Style::Text.call(I18n.t('titles.user_searches') % email, Style::TEXT_STYLES[:title])
-          make_rows(searches, table)
-        end
-      end
-
-      # @param [Hash] search
-      # @param [Terminal::Table] table
-      def fill_rules(search, table)
-        search['rules'].each do |rule|
-          table << [Style::Text.call(I18n.t("attributes.#{rule['name']}"), Style::TEXT_STYLES[:hint]),
-                    Style::Text.call(rule['value'].to_s, Style::TEXT_STYLES[:hint])]
+          fill_rows(searches, table)
         end
       end
 
       # @param [Array<Hash>] searches
       # @param [Terminal::Table] table
-      def make_rows(searches, table)
+      def fill_rows(searches, table)
         searches.each do |search|
           table << [Style::Text.call(I18n.t('attributes.total_quantity'), Style::TEXT_STYLES[:hint]),
                     Style::Text.call(search['total_quantity'].to_s, Style::TEXT_STYLES[:hint])]
-          fill_rules(search, table)
+          fill_rules(search['rules'], table)
           table << :separator
+        end
+      end
+
+      # @param [Array<Hash>] rules
+      # @param [Terminal::Table] table
+      def fill_rules(rules, table)
+        rules.each do |rule|
+          table << [Style::Text.call(I18n.t("attributes.#{rule['name']}"), Style::TEXT_STYLES[:hint]),
+                    Style::Text.call(rule['value'].to_s, Style::TEXT_STYLES[:hint])]
         end
       end
     end
